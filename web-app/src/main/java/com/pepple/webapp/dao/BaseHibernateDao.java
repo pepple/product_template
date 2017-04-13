7c31ae12de680c38ce8771f5fb51bc8124e93db7
pepple <pepple@126.com>
2017-04-13 08:53:25 +0800

    完美数据库操作基类

--- web-app/src/main/java/com/pepple/webapp/dao/BaseHibernateDao.java
+++ web-app/src/main/java/com/pepple/webapp/dao/BaseHibernateDao.java
@@ -1,10 +1,28 @@
 package com.pepple.webapp.dao;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * @description:
  * @author: pepple
  * @date: 2017/4/11 17:57
  */
-public interface BaseHibernateDao {
-    public <T> T getById(long id);
+public interface BaseHibernateDao<T,UniqueId extends Serializable> {
+    /**
+     * 根据主键获取对象
+     * @param id
+     * @return
+     */
+    public T get(UniqueId id);
+
+    public List<T> getAll(List list);
+
+    public boolean delete(UniqueId id);
+
+    public boolean deleteAll(List list);
+
+    public T add(T obj);
+
+    public T addAll(List<T> objList);
 }
--- web-app/src/main/java/com/pepple/webapp/dao/impl/BaseHibernateDaoImpl.java
+++ web-app/src/main/java/com/pepple/webapp/dao/impl/BaseHibernateDaoImpl.java
@@ -1,20 +1,54 @@
 package com.pepple.webapp.dao.impl;
 
 import com.pepple.webapp.dao.BaseHibernateDao;
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * @description:
  * @author: pepple
  * @date: 2017/4/11 17:59
  */
-public class BaseHibernateDaoImpl implements BaseHibernateDao {
-    @Autowired
-    private SessionFactory sessionFactory;
+public class BaseHibernateDaoImpl<T,UniqueId extends Serializable> extends HibernateDaoSupport implements BaseHibernateDao<T,UniqueId> {
+    protected Logger logger = Logger.getLogger(getClass());
+    private Class<T> pojoClass;
+
+    public BaseHibernateDaoImpl(Class<T> pojoClass) {
+        this.pojoClass = pojoClass;
+    }
+
     @Override
-    public <T> T getById(long id) {
-        sessionFactory.getCurrentSession().createQuery("");
+    public T get(UniqueId uniqueId) {
+        return getHibernateTemplate().get(pojoClass,uniqueId);
+    }
+
+    @Override
+    public List<T> getAll(List list) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(UniqueId uniqueId) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAll(List list) {
+        return false;
+    }
+
+    @Override
+    public T add(T obj) {
+        return null;
+    }
+
+    @Override
+    public T addAll(List<T> objList) {
         return null;
     }
 }
